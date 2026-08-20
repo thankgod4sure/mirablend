8eb6b21fe923961ece8aa1e798f3742281d3ed83
Thank God <wokomathankgod09@gmail.com>
2026-08-20 02:28:30 +0100

    Update Mira's Blend website

--- public/index.js
+++ public/index.js
@@ -20,7 +20,7 @@ menuCancel.addEventListener("click", () => {
     menuCancel.classList.remove("active");
 });
 
-//  window.addEventListener("load", function () {
+// window.addEventListener("load", function () {
 
 //     const loader = document.querySelector(".loader");
 
@@ -30,16 +30,16 @@ menuCancel.addEventListener("click", () => {
 
 //     }, 7000);
 
-// // });
-// const loader = document.querySelector(".loader");
-// const hero = document.querySelector("#hero");
-
-// window.addEventListener("load", () => {
-//     setTimeout(() => {
-//         loader.classList.add("hide");
-//         hero.classList.add("animate");
-//     }, 3000);
 // });
+const loader = document.querySelector(".loader");
+const hero = document.querySelector("#hero");
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loader.classList.add("hide");
+        hero.classList.add("animate");
+    }, 1000);
+});
 const orderBtns = document.querySelectorAll(".order-btn");
 const ordersBtns = document.getElementById(".orders-btn");
 const modal = document.getElementById("orderModal");
